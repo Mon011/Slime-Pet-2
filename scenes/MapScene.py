28f9c6505476ee895e5adafe47e585be0284d31b
Jakub Porzuczek <porzuczek.jakub@outlook.com>
2025-12-15 23:36:48 +0100

    Change map graphics

--- scenes/MapScene.py
+++ scenes/MapScene.py
@@ -7,27 +7,30 @@ from components import singleton, GameState
 
 class MapScene(Scene):
     background: Texture2D
-    back_button: Rectangle
-    shop_button: Rectangle
-    playground_button: Rectangle
+    back_button: Texture2D 
+    shop_button: Texture2D 
+    playground_button: Texture2D 
     BUTTON_WIDTH = 150
     BUTTON_HEIGHT = 60
 
     def load(self):
         self.background = load_texture(str(Path("assets/backgrounds/map-scene.png")))
-        self.back_button = Rectangle(SCREEN_WIDTH // 15 - self.BUTTON_WIDTH // 15, SCREEN_HEIGHT // 15, self.BUTTON_WIDTH, self.BUTTON_HEIGHT)
-        self.shop_button = Rectangle(SCREEN_WIDTH * 0.55 - self.BUTTON_WIDTH * 0.55, SCREEN_HEIGHT * 0.50, self.BUTTON_WIDTH, self.BUTTON_HEIGHT)
-        self.playground_button = Rectangle(SCREEN_WIDTH * 0.75 - self.BUTTON_WIDTH * 0.75, SCREEN_HEIGHT // 6, self.BUTTON_WIDTH, self.BUTTON_HEIGHT)
+        self.back_button = load_texture(str(Path("assets/buttons/back-button.png")))
+        self.shop_button = load_texture(str(Path("assets/buttons/shop-map-button.png")))
+        self.playground_button = load_texture(str(Path("assets/buttons/playland-map-button.png")))
 
     def render(self):
         clear_background(WHITE)
         draw_texture_ex(self.background, [0, 0], 0, Scale.QUADRUPLED, WHITE)
-        button.standard_button(self.back_button, 4, GRAY, BLACK, "Back", get_font_default(), navigate_to_town)
-        button.standard_button(self.shop_button, 4, GRAY, BLACK, "Shop", get_font_default(), navigate_to_shop)
-        button.standard_button(self.playground_button, 4, GRAY, BLACK, "Playground", get_font_default(), navigate_to_playground)
+        button.multiple_state_button(self.back_button, SCREEN_WIDTH // 16 - self.back_button.width // 4, SCREEN_HEIGHT // 15, Scale.TRIPLED, navigate_to_town)
+        button.multiple_state_button(self.shop_button, SCREEN_WIDTH * 0.6, SCREEN_HEIGHT * 0.2, Scale.TRIPLED, navigate_to_shop)
+        button.multiple_state_button(self.playground_button, SCREEN_WIDTH * 0.6, SCREEN_HEIGHT * 0.75, Scale.QUADRUPLED, navigate_to_playground)
 
     def unload(self):
         unload_texture(self.background)
+        unload_texture(self.back_button)
+        unload_texture(self.shop_button)
+        unload_texture(self.playground_button)
 
 def navigate_to_town():
     singleton.state = GameState.TOWN
